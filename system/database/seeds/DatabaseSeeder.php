6f4700a684a1e51a9dbb4c406987188227cdcc53
Capevace <capevace@gmail.com>
2018-06-11 15:00:00 +0200

    Fixed seeds

--- system/database/seeds/DatabaseSeeder.php
+++ system/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PackageSeeder::class);
-        $this->call(UserSeeder::class);
+        $this->call(TestUserSeeder::class);
     }
 }
--- system/database/seeds/TestUserSeeder.php
+++ system/database/seeds/TestUserSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class TestUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
