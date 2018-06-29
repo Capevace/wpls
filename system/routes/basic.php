8aa8cf91e855dbbfc305cc379978d088f781a302
Capevace <capevace@gmail.com>
2018-06-29 17:20:33 +0200

    Updater Working

--- system/routes/basic.php
+++ system/routes/basic.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
-use Symfony\Component\Console\Output\BufferedOutput;
+// Route::get('/install', 'InstallController@index')->name('setup:install-form');;
+// Route::post('/install', 'InstallController@install')->name('setup:install');
+// Route::get('/install/test', 'InstallController@testDatabase')->name('setup:test-db');
 
-Route::get('/install', 'InstallController@index')->name('setup:install-form');;
-Route::post('/install', 'InstallController@install')->name('setup:install');
-Route::get('/install/test', 'InstallController@testDatabase')->name('setup:test-db');
-
-Route::get('/update', function() {
-    $outputLog = new BufferedOutput;
-    Artisan::call('migrate', ['--force' => true], $outputLog);
-
-    return response('hello');
-});
+Route::get('/update', 'UpdateController@index')->name('update:index');
+Route::post('/update', 'UpdateController@update')->name('update:update');
